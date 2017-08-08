e767e9b80b755da959b5c903e8e16618ff9fa8ac
Steve Norum <stevenorum@gmail.com>
2017-08-07 23:38:12 -0400

    Adding basic exception functionality.

--- junko/exceptions.py
+++ junko/exceptions.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python3
 
+from junko.template_dispatchers import load_template
+
 class HttpException(Exception):
-    def __init__(self, template=None, code=None, message=None):
+    def __init__(self, template=None, code=None, params={}):
         self.template = template
         self.code = int(code)
-        self.message = message
+        self.params = params
         pass
 
     @classmethod
-    def from_code(cls, code, message=None):
-        return cls(tempalte="{code}.html".format(code=code), code=code, message=message)
+    def from_code(cls, code, params={}):
+        return cls(template="{code}.html".format(code=code), code=code, params=params)
     
-    def render(self, *args, **kwargs):
-        # TODO
-        return None
+    def render(self):
+        return load_template(self.template, params=self.params, code=self.code)
     pass
+
+def render_http_exceptions(function):
+    def newfunc(*args, **kwargs):
+        try:
+            return function(*args, **kwargs)
+        except HttpException as e:
+            return e.render()
+    return newfunc
